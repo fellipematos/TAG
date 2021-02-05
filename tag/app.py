1b7df1e72c7a10cc11a8d95b8301921c70fcdb87
fellipematos <fellipe.matos.37@hotmail.com>
2021-02-04 21:13:32 -0300

    Correções
    Corrigido campos do formulario, template layout dividido para futuros upgrades

--- tag/app.py
+++ tag/app.py
@@ -1,32 +1,32 @@
 from flask import Flask, render_template, request, url_for
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/etiqueta', methods=['GET', 'POST'])
 def tag():
-    if request.method == 'POST':
-        result = {
-                'Dnome': request.form['Dnome'],
-                'Dcep': request.form['Dcep'],
-                'Dendereco': request.form['Dendereco'],
-                'Dnum': request.form['Dnum'],
-                'Dcomplemento': request.form['Dcomplemento'],
-                'Dbairro': request.form['Dbairro'],
-                'Dcidade': request.form['Dcidade'],
-                'Destado': request.form['Destado'],
-                'Rnome': request.form['Rnome'],
-                'Rcep': request.form['Rcep'],
-                'Rendereco': request.form['Rendereco'],
-                'Rnum': request.form['Rnum'],
-                'Rcomplemento': request.form['Rcomplemento'],
-                'Rbairro': request.form['Rbairro'],
-                'Rcidade': request.form['Rcidade'],
-                'Restado': request.form['Restado']
-            }
 
-    return render_template('tag.html', result=result)
-    
+    if request.method == 'POST':
+        dados = {
+            'dnome': request.form['dnome'],
+            'dcep': request.form['dcep'],
+            'drua': request.form['drua'],
+            'dnum': request.form['dnum'],
+            'dcom': request.form['dcom'],
+            'dbairro': request.form['dbairro'],
+            'dcidade': request.form['dcidade'],
+            'duf': request.form['duf'],
+            'rnome': request.form['rnome'],
+            'rcep': request.form['rcep'],
+            'rrua': request.form['rrua'],
+            'rnum': request.form['rnum'],
+            'rcom': request.form['rcom'],
+            'rbairro': request.form['rbairro'],
+            'rcidade': request.form['rcidade'],
+            'ruf': request.form['ruf']
+        }
+    return render_template('tag.html', result=dados)
